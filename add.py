bfbadcb86c83d51dbf0ce79d241d31d172cc73d9
VARSHA GEORGE <20316@fisat>
2018-04-18 02:54:56 -0400

    create fn to return a no.

--- add.py
+++ add.py
@@ -11,8 +11,11 @@ def add4(num1,num2,num3,num4):
 def add5(num1,num2,num3,num4,num5):
 	s=num1+num2+num3+num4+num5
 	return s
+def add1(num1):
+	return s
 print add5(3,5,7,9,8)
 print add4(3,5,7,9)
 print add3(1,2,3)
 
 print add2(3,4)
+print add(3)
